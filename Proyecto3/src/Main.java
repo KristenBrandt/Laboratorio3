d56b36ea523f38d70456f8abb11554c2414ebf2b
KristenBrandt <kabrandtf@gmail.com>
2018-07-19 12:57:47 -0600

    Cambios en main

--- Proyecto3/src/Main.java
+++ Proyecto3/src/Main.java
@@ -22,35 +22,64 @@ public class Main {
             boolean on = true;
             Notepad note = new Notepad();
             String lis;
+            String tar;
 
 
+            /** primer menu - cuando no hay listas creadas **/
+            do {
+                System.out.println(opciones(note.getListaArraylist().isEmpty()));
+                if (note.getListaArraylist().isEmpty()) {
+                    String opcion = scan.nextLine();
+                    switch (opcion) {
+                        /** agregar tarea **/
+                        case "1":
+                            System.out.println("Ingrese el nombre de su lista de tareas");
+                            lis = scan.nextLine();
+                            Lista lista = new Lista(lis);
+                            note.agregarlista(lista);
+                            System.out.println("Ha agregado una lista exitosamente \n");
+                            for (Lista n : note.getListaArraylist()) {
+                                if (n.getNombre().equals(lis)) {
+                                    System.out.println("Ingrese una tarea para esa lista");
+                                    tar = scan.nextLine();
+                                    Tarea tarea = new Tarea(tar);
+                                    n.agregartarea(tarea);
+                                    System.out.println("Ha agregado una tarea exitosamente\n");
+                                    break;
+                                }
+                            break;}
+                            break;
 
-        do{
-            System.out.println(opciones(note.getListaArraylist().isEmpty()));
-            String opcion = scan.nextLine();
-            switch (opcion){
-                /** agregar tarea **/
-                case "1":
-                    System.out.println("Ingrese el nombre de su lista de tareas");
-                    lis = scan.nextLine();
-                    Lista lista = new Lista(lis);
-                    note.agregarlista(lista);
-                    System.out.println("Ha agregado una lista exitosamente \n");
-                    break;
+/** cerrar programa **/
+                        case "2":
+                            System.out.println("Cerrando Programa");
+                            on = false;
+                            break;
 
-                case "2":
-                    System.out.println("Cerrando Programa");
-                    on = false;
-                    break;
+                        /** por si usuario no ingresa alguno de los numeros deseados **/
+                        default:
+                            System.out.println("Debe de ingresar un numero valido, intente denuevo \n");
+                            break;
+                    }
+                }
+                do{
+                        String opcion = scan.nextLine();
+                        switch (opcion) {
+                            case "1":
+                                System.out.println("Ingrese el nombre de su lista de tareas");
+                                lis = scan.nextLine();
+                                Lista lista = new Lista(lis);
+                                note.agregarlista(lista);
+                                System.out.println("Ha agregado una lista exitosamente \n");
+                                break;
 
-                /** por si usuario no ingresa alguno de los numeros deseados **/
-                default:
-                    System.out.println("Debe de ingresar un numero valido, intente denuevo \n");
-                    break;
-            }
+                        }
 
+                    }
+                    while (on) ;
 
-        }while (on);
+            }while (on);
         }
     }
+
 
